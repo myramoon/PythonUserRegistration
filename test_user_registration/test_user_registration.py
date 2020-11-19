682e82a52f65ca6efdbce49dd4541771f2e1b656
anamfazal <anamfazal94@gmail.com>
2020-11-19 23:13:05 +0530

    [myramoon]Added: test case to validate email

--- test_user_registration/test_user_registration.py
+++ test_user_registration/test_user_registration.py
@@ -16,3 +16,31 @@ def test_validate_name_raises_exception_on_invalid_name(instance_of_user_registr
     with pytest.raises(InvalidInput):
         instance_of_user_registration.validate_name("A3jhsfd")
 
+
+@pytest.mark.parametrize("input , expected" , [
+                ("abc@yahoo.com", True),
+                ("abc-100@yahoo.com", True),
+                ("abc.100@yahoo.com", True),
+                ("abc111@abc.com", True),
+                ("abc-100@abc.net", True),
+                ("abc.100@abc.com.au", True),
+                ("abc@1.com", True),
+                ("abc@gmail.com.com", True),
+                ("abc+100@gmail.com", True),
+                ("abc", False),
+                ("abc@.com.my", False),
+                ("abc123@.com", False),
+                ("abc123@.com.com", False),
+                ("abc123@gmail.a", False),
+                (".abc@abc.com", False),
+                ("abc()*@gmail.com", False),
+                ("abc@%*.com", False),
+                ("abc..2002@gmail.com", False),
+                ("abc.@gmail.com", False),
+                ("abc@abc@gmail.com", False),
+                ("abc@gmail.com.1a", False),
+                ("abc@gmail.com.aa.au", False)
+])
+def test_validate_email(input ,expected , instance_of_user_registration):
+    result = instance_of_user_registration.validate_email(input)
+    assert result == expected
