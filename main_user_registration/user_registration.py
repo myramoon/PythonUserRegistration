c7ad928f325abe2ec39514680d9102c39bf9223e
anamfazal <anamfazal94@gmail.com>
2020-11-19 23:12:45 +0530

    [myramoon]Added: functionality code to validate email

--- main_user_registration/user_registration.py
+++ main_user_registration/user_registration.py
@@ -10,3 +10,8 @@ class UserRegistration():
         if not match: raise InvalidInput('Please enter name with minimum 3 characters and beginning with upper case')
         else: return match.group()
 
+    def validate_email(self , email):
+        pattern = re.compile(r'^[A-Za-z0-9]+([-.+_]{1}[0-9A-Za-z]+)*@[A-Za-z0-9]+.[a-zA-Z]{2,4}([.,]{1}[a-z]{2,3}){0,1}$')
+        match = pattern.search(email)
+        if not match: return False
+        else: return True
