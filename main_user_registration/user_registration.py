9dd4f45a1d68f90eb4c814ba65810d97f13020f8
anamfazal <anamfazal94@gmail.com>
2020-11-19 23:57:22 +0530

    [myramoon]Added: functionality code to validate phone number

--- main_user_registration/user_registration.py
+++ main_user_registration/user_registration.py
@@ -10,6 +10,14 @@ class UserRegistration():
         if not match: raise InvalidInput('Please enter name with minimum 3 characters and beginning with upper case')
         else: return match.group()
 
+    def validate_phone_number(self, phone_number):
+        pattern = re.compile(r'^([1-9][0-9]){1}[-\s][1-9]{1}[0-9]{9}$')
+        match = pattern.search(phone_number)
+        if not match:
+            raise InvalidInput('Please enter number in proper format: [2-digit country code](space/-) [10 digit mobile number]')
+        else:
+            return match.group()
+
     def validate_email(self , email):
         pattern = re.compile(r'^[A-Za-z0-9]+([-.+_]{1}[0-9A-Za-z]+)*@[A-Za-z0-9]+.[a-zA-Z]{2,4}([.,]{1}[a-z]{2,3}){0,1}$')
         match = pattern.search(email)
